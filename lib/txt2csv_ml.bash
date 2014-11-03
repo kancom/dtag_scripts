2d4520be1e90787046602d75f110d5a3e461b4c3
Andrey Kashirin <kas@rivasense.com>
2014-11-03 12:49:02 +0300

    spec rtrv-map in file

--- lib/txt2csv_ml.bash
+++ lib/txt2csv_ml.bash
@@ -3,9 +3,9 @@
 cd $1;
 
 awk '
-BEGIN {body=0;}
 {
   if ($0 ~ /> rtrv-gta/) {
+    body=0;
     cmd=gensub(/[^a-z0-9\-]/, "", "g");
     fname = cmd ".csv";
     print "START GTA,END GTA,XLAT,RI,ITU PC,MRNSET/MAPSET,SSN,CCGT,CGGTMOD,GTMODID,LOOPSET,OPTSN,CGSELID,OPCSN,ACTSN,PPMEASREQD" > fname;
@@ -24,6 +24,36 @@ BEGIN {body=0;}
     } else if ($0 ~ /[A-Z]+=/) {
       $0 = gensub(/[A-Z]+=/, "", "g");
       printf ",%s", $0 >> fname;
+    }
+  }
+}
+' "${2}"
+
+awk '
+BEGIN {body=0;}
+{
+  if ($0 ~ /> rtrv-map/) {
+    fname = "rtrv-map.csv";
+    print "PCN,Mate PCN,SSN,RC,MULT,SRM,MRC,GRP NAME,SSO,WT,%WT,THR,MAPSET ID,MRNSET ID,MRNPC(N)"  > fname;
+  }
+  if ($0 ~ /MAPSET ID.*MRNSET ID.*MRNPC/) {
+    body=1;
+    mapset=gensub(/^ *MAPSET ID=([^ ]+) .*/, "\\1", "g");
+    mrnset=gensub(/^.*MRNSET ID=([^ ]+) .*/, "\\1", "g");
+    mrnpc=gensub(/^.*MRNPCN? *= *([^ ]+)$/, "\\1", "g");
+  } else if (body) {
+    $0 = gensub(/^ {7}| +$/, "", "g");
+    $0 = gensub(/([^=]) +([A-Z]+=)/, "\\1,\\2", "g");
+    if ($0 ~ /Command/) {$0=""}
+    if ($0==";") {
+      body=0;
+    } else if ($0 ~ /^[a-z0-9]+/) {
+      $0 = gensub(/ {8,}/, ",,", "g");
+      $0 = gensub(/ +/, ",", "g");
+      printf "%s,%s,%s,%s\n",$0,mapset,mrnset,mrnpc >> fname;
+    } else if ($0 ~ /^ +[a-z0-9]+/) {
+      $0 = gensub(/ +/, ",", "g");
+      printf "%s\n",$0 >> fname;
     }
   }
 }
